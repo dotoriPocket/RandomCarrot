ace3a8d91cd5d112436934c6d44b148839118ff2
dotoriPocket <monparis12@naver.com>
2023-03-03 00:23:50 +0900

    addBox

---
+++ choose.js
@@ -0,0 +1,18 @@
+
+const url = window.location.href;
+
+let value = url.split("?")[1];
+let num = parseInt(value.split("=")[1]);
+
+let boxGuide = document.getElementsByClassName("box_guide")[0];
+
+
+
+for(let n = 1; n <= num; n = n + 1) {
+    let numBox = document.createElement("div");
+
+    numBox.setAttribute("class","num_box");
+    numBox.innerText = n;
+    
+    boxGuide.appendChild(numBox);
+}
